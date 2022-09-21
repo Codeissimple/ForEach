5cc69ef035e96f54c56c9b4f08b95df3e9e71290
Codeissimple <adam.jkirjacev@gmail.com>
2022-09-21 19:23:51 -0400

    1

--- script.js
+++ script.js
@@ -34,3 +34,17 @@ for (let price of prices) {
     total +=price;
 }
 console.log(total);
+
+
+
+
+function forEach(arr, callback) {
+    for(let i=0; i < arr.length; i++) {
+        callback(arr[i], i);
+    }
+    return undefined;
+};
+
+forEach(colors, function(color, i) {
+    console.log(color.toUpperCase(), ' at index of ', i);
+});
