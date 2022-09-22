2a537dd441f5ca181f79e33607e95cd726f46141
Codeissimple <adam.jkirjacev@gmail.com>
2022-09-21 20:52:09 -0400

    1

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 let doubleValues = [1,2,3,4,5,6,7,8,9];
-doubleValues.map(function(value, index, array) {
+const doubleValuesOutput =doubleValues.map(function(value, index, array) {
     return value * 2;
-})
+});
