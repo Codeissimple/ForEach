391b371e869308177dccdc48bb06a39164c1280d
Codeissimple <adam.jkirjacev@gmail.com>
2022-09-21 21:05:33 -0400

    1

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
-let doubleValues = [1,2,3,4,5,6,7,8,9];
-const doubleValuesOutput =doubleValues.map(function(value, index, array) {
-    return value * 2;
-});
+
+simpleArray = [1,2,3,4,5,6,7,8,9];
+
+function doubleValues(arr) {
+    let newArray = [];
+    arr.forEach(function(val) {
+        newArray.push(val *2);
+    });
+    return newArray;
+}
+
+console.log(doubleValues(simpleArray));
+
+function onlyEvenValues(arr) {
+    let newArray = [];
+    arr.forEach(function(val) {
+        if(val % 2 == 0){
+            newArray.push(val);
+        }
+    });
+    return newArray;
+};
+
+console.log(onlyEvenValues(simpleArray));
+
+showFirstAndLast(arr) {
+    let newArray = [];
+    arr.forEach(function(val) {
+        
+    })
+}
